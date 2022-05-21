5f71a5d7b5be1b5089df25f8d7d50e566199a52b
hidrigin <98439047+hidrigin@users.noreply.github.com>
2022-05-21 19:52:35 +0800

    Update set_passwd.bat

--- set_passwd.bat
+++ set_passwd.bat
@@ -4,5 +4,5 @@ set safepwd=1qaz123!@#$
 set path="C:\Program Files (x86)\ToDesk\ToDesk.exe"
 
 echo.
-for /f "delims=" %%i in ('%path% -getid') do echo ID:%%i
+for /f "delims=" %%i in ('%path% -status') do echo %%i
 for /f "delims=" %%i in ('%path% -setpasswd %safepwd%') do echo Change the security password to %safepwd% 
